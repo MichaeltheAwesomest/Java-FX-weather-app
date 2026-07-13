3703371b5661e8ff05feb53209c8e3540dda1468
yhungboss-web <maroakpobasah@gmail>
2026-07-13 20:54:02 +0100

    Added an AppClient class to make API requests

--- src/application/AppWebClient.java
+++ src/application/AppWebClient.java
@@ -1,5 +1,34 @@
 package application;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class AppWebClient {
 
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public String getIpInformation() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ipapi.co/json/"))
+                .header("User-Agent", "Java-HttpClient") 
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(
+                    request, 
+                    HttpResponse.BodyHandlers.ofString()
+            );
+
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Error fetching IP information: " + e.getMessage());
+            if (e instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+            return null;
+        }
+    }
 }
