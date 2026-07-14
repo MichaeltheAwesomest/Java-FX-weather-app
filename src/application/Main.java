3c70c5f75942d1e8b3a7d4bc202c47e81105d5d7
mikeytheawesomest <mikeytheawesomest@gmail.com>
2026-07-14 09:20:48 +0100

    Made a search bar , started working on the main Ui interface , preparing for yhung boss to implenet get weather response

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,60 +1,113 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import open_weather_response.WeatherResponse;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
-
 public class Main extends Application {
 	AppIpClient appIpClient = new AppIpClient();
-	WeatherResponse weatherResponse = null;
+	Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
-			Scene loadLocalWeatherDataScene = loadLocalWeatherDataScene();
-			primaryStage.setScene(loadLocalWeatherDataScene);
-			primaryStage.show();
+		this.primaryStage = primaryStage;
+		Scene loadLocalWeatherDataScene = loadLocalWeatherDataScene();
+		primaryStage.setScene(loadLocalWeatherDataScene);
+		primaryStage.setTitle("Weather Dash Board App.");
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public Scene loadLocalWeatherDataScene() {
 		ProgressIndicator loadingSpinner = new ProgressIndicator(ProgressIndicator.INDETERMINATE_PROGRESS);
 		loadingSpinner.setMaxSize(60, 60);
 		Label taskLabel = new Label("Getting Ip information.");
 		taskLabel.setTextAlignment(TextAlignment.CENTER);
-		
+
 		VBox content = new VBox(10, loadingSpinner, taskLabel);
 		content.setAlignment(Pos.CENTER);
-		
+
 		BorderPane root = new BorderPane();
 		root.setCenter(content);
-		Scene scene = new Scene(root,400,400);
+		Scene scene = new Scene(root, 400, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		
-		
-		Thread localWeatherInfoThread = new Thread(()->{
+
+		Thread localWeatherInfoThread = new Thread(() -> {
 			IpInformation ipInformation = appIpClient.getIpInformation();
-			Platform.runLater(()->{
+			Platform.runLater(() -> {
 				taskLabel.setText("Got Ip information.\nGetting weather Information.");
 			});
-			weatherResponse = appIpClient.getWeatherResponse(null,ipInformation);
-			Platform.runLater(()->{
+			WeatherResponse weatherResponse = appIpClient.getWeatherResponse(null, ipInformation);
+			Platform.runLater(() -> {
 				taskLabel.setText("Done");
+				primaryStage.setScene(weatherDataScene(weatherResponse));
 			});
 		});
-		
+
 		localWeatherInfoThread.start();
 		return scene;
 	}
+
+	public Scene weatherDataScene(WeatherResponse weatherResponse) {
+		ToggleButton darkModeToggle = new ToggleButton("toggle mode");
+
+		Button weatherAtLocationButton = new Button("Current Location");
+
+		VBox searchBar = new SearchBarVBox();
+
+		HBox.setHgrow(searchBar, Priority.ALWAYS);
+
+		HBox header = new HBox(30, darkModeToggle, searchBar, weatherAtLocationButton);
+
+		GridPane gridPane = new GridPane(20, 20);
+
+		String cityName = weatherResponse.city.name;
+		Label cityNameLabel = new Label(cityName);
+
+		LocalDateTime currentTime = LocalDateTime.now();
+
+		DateTimeFormatter hourMinuteFormatter = DateTimeFormatter.ofPattern("H:mm");
+		String hourMinuteString = hourMinuteFormatter.format(currentTime);
+		Label hourMinuteLabel = new Label(hourMinuteString);
+
+		DateTimeFormatter monthDayFormatter = DateTimeFormatter.ofPattern("EEEEd',' MMMM");
+		String monthDayString = monthDayFormatter.format(currentTime);
+		Label monthDayLabel = new Label(monthDayString);
+
+		Region vSpacer = new Region();
+		vSpacer.setPrefHeight(40);
+
+		VBox cityTimeGrid = new VBox(10, cityNameLabel, hourMinuteLabel, vSpacer, monthDayLabel);
 		
+		gridPane.add(cityTimeGrid, 0, 0);
+
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(20));
+		root.setTop(header);
+		root.setCenter(gridPane);
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		return scene;
+	}
 }
---
+++ src/application/SearchBarVBox.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+public class SearchBarVBox extends VBox {
+	public SearchBarVBox() {
+		TextField searchTextField = new TextField();
+		searchTextField.setPromptText("Search for a city...");
+		HBox.setHgrow(searchTextField, Priority.ALWAYS);
+		
+		ObservableList<String> masterData = FXCollections.observableArrayList("Apple", "Banana", "Cherry", "Date",
+				"Elderberry", "Fig", "Grape");
+
+		FilteredList<String> filteredData = new FilteredList<>(masterData, _ -> false);
+		
+		final ListView<String> listView = new ListView<>();
+		listView.setVisible(false);
+
+		searchTextField.textProperty().addListener((_, _, newValue) -> {
+			filteredData.setPredicate(item -> {
+				if (newValue.length() < 3) {
+					listView.setVisible(false);
+					return false;
+				}
+
+				String lowerCaseFilter = newValue.toLowerCase();
+				if(item.toLowerCase().contains(lowerCaseFilter)) {
+					listView.setVisible(true);
+					return true;
+				}else {
+					listView.setVisible(false);
+				}
+				return false;
+			});
+		});
+
+		
+		listView.setItems(filteredData);
+		super(10,searchTextField, listView);
+	}
+}
